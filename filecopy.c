6015ccea242774870e9383525abc8295df313bbf
Bikramjit <bikramjit.garcha@outlook.com>
2018-10-03 13:40:24 -0700

    Fixed the errors and commented ;)

--- filecopy.c
+++ filecopy.c
@@ -1,3 +1,8 @@
+/*
+Assignment 2: Question #2
+Bikramjit Garcha (991330905)
+Harmanjeet Singh (991439186)
+*/
 
 #include<stdio.h> 
 #include<stdlib.h> 
@@ -5,6 +10,7 @@
 #include<sys/types.h> 
 #include<string.h> 
 #include<sys/wait.h>
+#include<fcntl.h>
 
 int main(int argc, char *argv[]){
 
@@ -15,29 +21,36 @@ int main(int argc, char *argv[]){
 		char buff[1500];
 		int counter;
 
-		pipe(fd)
+		pipe(fd);
 
-		//stores file names in a variable
+		//stores and opens file name into a variable
 		int inputFile = open(argv[1], 0);
 		int copyFile = open(argv[2], 0666);
 
+		// errorchecking to see if both files have opned
 		if(inputFile == -1 || copyFile == -1){
 			printf("Failed to open file");
 			exit(1);
 		}
+
+		//child process 
 		child=fork();
 
 		if(child == 0){
 			close(fd[1]);
 			read(fd[0], buff, sizeof(buff));
-
+			
+			//writing to the copyFIle
 			write(copyFile, buff, strlen(buff));
 		}else{
 			close(fd[0]);
-
-			while((counter=read(inputFile, buffm sizeof(buff))>0)){
+			
+			// read from file
+			while((counter=read(inputFile, buff, sizeof(buff))>0)){
 				write(fd[1], buff, sizeof(buff));
 			}
+
+			// close and exit 
 			close(fd[1]);
 		}
 	}else{
